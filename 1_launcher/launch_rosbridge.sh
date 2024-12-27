ac96cfb93a9e5522249511554b7e35dd8670f801
kasekiguchi <kasekiguchi@gmail.com>
2024-12-27 10:29:56 +0900

    Update launch_rosbridge.sh

--- 1_launcher/launch_rosbridge.sh
+++ 1_launcher/launch_rosbridge.sh
@@ -2,4 +2,4 @@
 
 source /common/scripts/super_echo
 echo "ROS bridge"
-$(echo "exec ros2 launch rosbridge_server rosbridge_websocket_launch.xml --ros-args --remap __ns:=/${HOSTNAME}")
+$(echo "exec ros2 launch rosbridge_server rosbridge_websocket_launch.xml")
