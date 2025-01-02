05b40871010e331cd5415d84bd54940816a1e1eb
kasekiguchi <kasekiguchi@gmail.com>
2025-01-03 00:43:33 +0900

    trail comp

--- 1_launcher/launch_GUI.sh
+++ 1_launcher/launch_GUI.sh
@@ -2,5 +2,4 @@
 
 source /common/scripts/super_echo
 echo "NPM START"
-#$(echo "exec chmod a+x ./start_GUI.sh && ./start_GUI.sh")
-eval "if [[ ! -d node_modules ]];then npm i; fi; npm run dev"
+eval "if [[ ! -d node_modules ]];then echo 'NPM INSTALL'; npm i; fi; npm run dev"
--- GUI/start_GUI.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-
-source /common/scripts/super_echo
-if [[ ! -d node_modules ]]; then
-  echo "SET UP GUI"
-  npm i
-fi
-npm run dev
