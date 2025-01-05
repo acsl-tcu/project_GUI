f352b3677efbd00c2343ef07eca5195d9f5ec218
kasekiguchi <kasekiguchi@gmail.com>
2025-01-05 13:50:30 +0900

    chromium

--- 1_launcher/launch_chrome.sh
+++ 1_launcher/launch_chrome.sh
@@ -2,4 +2,5 @@
 
 source /common/scripts/super_echo
 echo "KIOSK START"
-$(echo "exec google-chrome-stable --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
+#$(echo "exec google-chrome-stable --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
+$(echo "exec chromium --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
