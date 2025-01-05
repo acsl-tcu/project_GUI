e7f453c52d72f8327735610eed504bbcba035d78
kasekiguchi <kasekiguchi@gmail.com>
2025-01-05 16:26:36 +0900

    GUI

--- 1_launcher/launch_chrome.sh
+++ 1_launcher/launch_chrome.sh
@@ -3,4 +3,11 @@
 source /common/scripts/super_echo
 echo "KIOSK START"
 #$(echo "exec google-chrome-stable --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
+if id -u $hostUID >/dev/null 2>&1; then
+  username=$(getent passwd 1000 | sed 's/:.*//' else)
+  su ${username}
+else
+  adduser -m -u $hostUID dstudent
+  su dstudent
+fi
 $(echo "exec chromium --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
