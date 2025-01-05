bee763c888e39697472d5a77c5d1d00d45414b71
kasekiguchi <kasekiguchi@gmail.com>
2025-01-05 17:18:08 +0900

    kiosk

--- 1_launcher/launch_chrome.sh
+++ 1_launcher/launch_chrome.sh
@@ -3,14 +3,14 @@
 source /common/scripts/super_echo
 echo "KIOSK START"
 #$(echo "exec google-chrome-stable --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
-if id -u ${hostUID} >/dev/null 2>&1; then
-  echo "== user exists ======================"
-  username=$(getent passwd ${hostUID} | sed 's/:.*//')
-  su ${username}
-else
-  echo "== user add ====================="
-  groupadd -g ${hostGID} acslgroup
-  adduser -m -u ${hostUID} -g ${hostGID} dstudent
-  su dstudent
-fi
+# if id -u ${hostUID} >/dev/null 2>&1; then
+#   echo "== user exists ======================"
+#   username=$(getent passwd ${hostUID} | sed 's/:.*//')
+#   su ${username}
+# else
+#   echo "== user add ====================="
+#   groupadd -g ${hostGID} acslgroup
+#   adduser -m -u ${hostUID} -g ${hostGID} dstudent
+#   su dstudent
+# fi
 $(echo "exec chromium --enable-features=WebUIDarkMode --force-dark-mode http://localhost:3000 --kiosk --no-sandbox")
