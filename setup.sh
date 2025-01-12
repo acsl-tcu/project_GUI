cfe757c301b6dac5958c886918dc1c4590630cfc
kasekiguchi <kasekiguchi@gmail.com>
2025-01-12 17:13:02 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ sed -i "s|\$ACSL_ROS2_DIR|$ACSL_ROS2_DIR|g" project_launch*.sh
 
 dup GUI
 
+source $ACSL_ROS2_DIR/4_docker/common/scripts/super_echo
+
+gecho Set autostart file in $HOME/.local/share/applications
 sed -i "s|ACSL_WORK_DIR|${ACSL_WORK_DIR}|" ./start_kiosk.desktop
 desktop-file-install --dir=$HOME/.local/share/applications ./start_kiosk.desktop
 update-desktop-database -v ~/.local/share/applications
 
-source $ACSL_ROS2_DIR/4_docker/common/scripts/super_echo
 gecho After display "Ready", reboot the system
 dlogs GUI
