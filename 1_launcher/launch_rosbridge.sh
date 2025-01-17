d4ad0827066aac5cf8c0ed0f565427a2b2dcfa7c
kasekiguchi <kasekiguchi@gmail.com>
2025-01-17 16:34:03 +0900

    Update launch_rosbridge.sh

--- 1_launcher/launch_rosbridge.sh
+++ 1_launcher/launch_rosbridge.sh
@@ -2,4 +2,7 @@
 
 source /common/scripts/super_echo
 echo "ROS bridge"
-$(echo "exec ros2 launch rosbridge_server rosbridge_websocket_launch.xml")
+export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
+export ROS_STATIC_PEERS=$(ip a | grep 192 | grep -o 'inet [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o [0-9].*)
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml
+#$(echo "exec ros2 launch rosbridge_server rosbridge_websocket_launch.xml")
