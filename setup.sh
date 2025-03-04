aa7d2e0f2bab435e73e6fbc37c767d5725bc390b
kasekiguchi <kasekiguchi@gmail.com>
2025-03-04 19:06:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # https://wiki.archlinux.jp/index.php/%E3%83%87%E3%82%B9%E3%82%AF%E3%83%88%E3%83%83%E3%83%97%E3%82%A8%E3%83%B3%E3%83%88%E3%83%AA
 # https://forums.raspberrypi.com/viewtopic.php?t=108958
+gpull
 sed -i "s|\$ACSL_WORK_DIR|$ACSL_WORK_DIR|g" project_launch*.sh
 sed -i "s|\$ACSL_ROS2_DIR|$ACSL_ROS2_DIR|g" project_launch*.sh
 sed -i "s|RID_IN_PAGE|$ROS_DOMAIN_ID|" GUI/app/page.tsx
